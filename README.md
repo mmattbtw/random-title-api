b0961cf6d4105957e3e031677e36a65605f68ee1
mmattbtw <matt@mmatt.net>
2022-01-31 22:03:14 -0600

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# random-title-api
+
+get a random title from a list of markov chained titles from the likes of xQc and Forsen.
+
+## Documentation
+
+- `GET` `/`
+  - Response:
+  - ```
+    {
+        "title": "random title",
+        "status": "200",
+    }
+    ```
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const config = require('./config');
+const axios = require('axios');
+const rateLimit = require('express-rate-limit')
+
+const app = express()
+
+const PORT = config.port || 3000;
+
+app.use(express.json())
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+})
+
+app.use(limiter)
+
+app.get('/', async function (req, res) {
+    const titles = (await axios.get('https://gist.githubusercontent.com/MrAuro/57dc83822707119bf07085408bddf4d0/raw/061b7f2c7545d92a6d80adb9cfff9aef85d34b4c/titles.txt')).data;
+    const titlesArray = titles.split('\n');
+    const randomTitle = titlesArray[Math.floor(Math.random() * titlesArray.length)];
+    
+    res.status(200).json({
+        "title": randomTitle,
+        "status": "200"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`🎉 Random Title API is running on port ${PORT}`);
+})
