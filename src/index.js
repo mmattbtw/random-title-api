fb58cba85561922ae27a88e0b8126b144c123894
matt <30363562+mmattbtw@users.noreply.github.com>
2022-02-20 20:28:11 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const PORT = config.port || 3000;
 
 app.use(express.json())
 
+const GIST_URL = 'https://gist.githubusercontent.com/mmattbtw/91312bce7be2d89b9d21cc8ba050fc2d/raw/915bb3ba7759f4380076b0d37359e8698145b229/new_titles.txt'
+
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
@@ -20,7 +22,7 @@ const limiter = rateLimit({
 app.use(limiter)
 
 app.get('/string', async function (req, res) {
-    const titles = (await axios.get('https://gist.githubusercontent.com/mmattbtw/91312bce7be2d89b9d21cc8ba050fc2d/raw/0a9c22b26c2cdbb9ff24ce8c1a78f7ed8b267839/new_titles.txt')).data;
+    const titles = (await axios.get(GIST_URL)).data;
     const titlesArray = titles.split('\n');
     const randomTitle = titlesArray[Math.floor(Math.random() * titlesArray.length)];
     
@@ -28,7 +30,7 @@ app.get('/string', async function (req, res) {
 })
 
 app.get('/json', async function (req, res) {
-    const titles = (await axios.get('https://gist.githubusercontent.com/mmattbtw/91312bce7be2d89b9d21cc8ba050fc2d/raw/0a9c22b26c2cdbb9ff24ce8c1a78f7ed8b267839/new_titles.txt')).data;
+    const titles = (await axios.get(GIST_URL)).data;
     const titlesArray = titles.split('\n');
     const randomTitle = titlesArray[Math.floor(Math.random() * titlesArray.length)];
     
